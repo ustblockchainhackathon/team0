c87e372077b626c63a4129eaa11ed03a70bebf89
Your Name <you@example.com>
2016-11-26 16:37:24 +0100

    fix postObJT TO REGISTER

--- blockchain-passport-mobile/www/js/controllers/RegisterCtrl.js
+++ blockchain-passport-mobile/www/js/controllers/RegisterCtrl.js
@@ -5,11 +5,14 @@
 		.module('starter.Register')
 		.controller('RegisterCtrl', RegisterCtrl);
 
-	function RegisterCtrl($scope, $stateParams, ionicMaterialInk,NotificationsFactory,$state) {
+	function RegisterCtrl($scope, $stateParams, ionicMaterialInk,NotificationsFactory,$state,NotificationsService) {
 		//ionic.material.ink.displayEffect();
 		ionicMaterialInk.displayEffect();
         $scope.registerAccount= function(){ 
-            NotificationsFactory.postRegister("0xc30deb0bc575a1335afbbc3af1a2286874f79f95324afcd0ca1a86f2751296fb").then(function () {
+          var objToPost={
+              "ethereumAddress":"0xc30deb0bc575a1335afbbc3af1a2286874f79f95324afcd0ca1a86f2751296fb",
+              "registrationToken": NotificationsService.getToken()
+          }  NotificationsFactory.postRegister(objToPost).then(function () {
 				NotificationsFactory.setAddress("0xc30deb0bc575a1335afbbc3af1a2286874f79f95324afcd0ca1a86f2751296fb");
 						console.log('succes')
 	            $state.go("app.admin")
